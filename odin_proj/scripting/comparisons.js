fe684e5c2a31e881d76713766d66598b569d7a6d
nvhiii <nahikhan16@gmail.com>
2024-04-05 21:22:44 -0400

    added some js conditional practice

---
+++ odin_proj/scripting/comparisons.js
@@ -0,0 +1,13 @@
+5 > 4; // returns true bc 5 larger than 4
+
+"apple" > "pineapple"; // returns false, because the index of a index is less than p
+
+"2" > "12"; // returns false, type conversion to nums and then 2 is not greater than 12
+
+undefined == null; // returns true, bc type conversion renders them equal
+
+undefined === null; // returns false, bc no type conversion n not strictly equal
+
+null == "\n0\n"; // returns false, null is not equal to anything other than undefined
+
+null === +"\n0\n"; // returns false, type conversion wise they are not equal
